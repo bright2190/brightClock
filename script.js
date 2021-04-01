9e4fbf11b0080d4724971292c7163bb9872e3034
Bright Emmanuel <ghena.bright@gmail.com>
2021-04-01 09:37:38 +0100

    added readme

---
+++ script.js
@@ -0,0 +1,29 @@
+function brightTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 12){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("brightClock").innerText = time;
+    document.getElementById("brightClock").textContent = time;
+    
+    setTimeout(brightTime, 1000);
+    
+}
+
+brightTime();
